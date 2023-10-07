417c452f9c0e2ff1ca5c1420d901d84092dff18f
guyroW <halimriabi001@gmail.com>
2023-10-07 15:17:39 +0100

    Adding a DatePicker and OnDateSetListener

--- app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
@@ -1,11 +1,41 @@
 package eu.tutorials.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+        {view, year, month, dayOfMonth ->
+
+            Toast.makeText(this,
+                "Date Picker Works!", Toast.LENGTH_SHORT).show()
+        },
+        year,
+        month,
+        day
+        ).show()
+
+
     }
 }
